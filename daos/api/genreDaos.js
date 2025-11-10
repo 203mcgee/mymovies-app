966223f33a0244986de3d77f6e3884d3c77ba068
Zoe McGee <zoe.c.mcgee@gmail.com>
2025-11-10 11:58:07 -0600

    first commit

--- daos/api/genreDaos.js
+++ daos/api/genreDaos.js
@@ -1,7 +1,16 @@
 const connect = require('../../config/dbconfig')
 
 const genreDaos = {
-    table: 'genre'
+    table: 'genre',
+    findByGenre: (res,table,id) => {
+        let sql = `select * from movie_to_${table} join movie using (movie_id) join ${table} using (${table}_id) where ${table}_id = ${id};`
+        connect.execute(
+          sql,
+          
+            
+        )
+
+    } 
 }
 
 module.exports = genreDaos
--- daos/api/streamingPlatformDaos.js
+++ daos/api/streamingPlatformDaos.js
@@ -1,10 +1,7 @@
 const connect = require('../../config/dbconfig')
 
 const streamingPlatformDaos ={
-    table: 'streaming_platform',
-    movieOnStreamingPlatform: (res,table,id) => {
-      const sql = `select * from movie m join ${table}`  
-    }
+    table: 'streaming_platform'
 }
 
 module.exports = streamingPlatformDaos
